90226e01d81e92c8be35cd706afafddb1b0fd950
wilker22 <wilker.silva@codevasf.gov.br>
2025-04-24 14:30:04 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo "Hello world";
+});
+
+Route::get('/about', function () {
+    echo "About us";
 });
