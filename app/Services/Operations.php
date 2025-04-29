1e1ff32420aa4ece086a89b3bfa38ff56d18ba42
wilker22 <wilker.silva@codevasf.gov.br>
2025-04-28 22:09:09 -0300

    correção do decrypt

--- app/Services/Operations.php
+++ app/Services/Operations.php
@@ -13,7 +13,7 @@ class Operations
         try {
             $value = Crypt::decrypt($value);
         } catch (DecryptException $e) {
-            return redirect()->route('home');
+            return null;
         }
 
         return $value;
